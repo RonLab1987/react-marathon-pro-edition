5f25426699dbb7c2d3ac36e549b8cb8ff78b0c50
Roman Laptev <rlaptev@infoss.no>
2020-11-02 15:08:53 +0300

    Add lint flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import * as React from 'react'
-import cn from 'classnames'
+import React from "react";
+import cn from "classnames";
 
-// @ts-ignore
-import s from './App.modules.scss'
+import s from "./App.module.scss";
 
-export const App: React.FC = () => {
+const App: React.FC = () => {
   return (
     <div className={s.container}>
-      <h1 className={cn(s.greeting, 'text-center', 'fill-width')}>I'am App</h1>
+      <h1 className={cn(s.greeting, "text-center", "fill-width")}>
+        I&apos;am App
+      </h1>
     </div>
-  )
-}
+  );
+};
+
+export default App;
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import "./text-utils.scss";
+import "./size-utils.scss";
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
